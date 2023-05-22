dcf1b795f30481103c3fcf100fda5ad3040eb932
Asad-Kareem <kareembakhs112244@gmail.com>
2023-05-22 04:19:38 -0700

    context

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Modal from "./Modal";
 import { useQuery } from "@tanstack/react-query";
 import ErrorBoundry from "./ErrorBoundry";
 import Carasoul from "./Carosouls";
 import fetchPet from "./fetch";
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate;
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
   const results = useQuery(["details", id], fetchPet);
   if (results.isLoading) {
     return (
@@ -23,8 +29,38 @@ const Details = () => {
         <h2>
           {pets.animal}-{pets.breed} -{pets.city} -{pets.state}
         </h2>
-        <button>Adopt {pets.name}</button>
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          Adopt {pets.name}
+        </button>
         <p>{pets.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pets.name}?</h1>
+              <div>
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pets);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
+                <button
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
